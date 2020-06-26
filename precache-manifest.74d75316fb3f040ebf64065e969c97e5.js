a76c188711be4e70701e4829ec062edb94f1c2f5
Anubhuv Kumar <100expertdays@gmail.com>
2020-06-26 17:58:30 +0530

    Updates

---
+++ precache-manifest.74d75316fb3f040ebf64065e969c97e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1c3ec2305156e5bb8d8c67ac025333",
+    "url": "/wcovid19/index.html"
+  },
+  {
+    "revision": "2be568a9e89de7dcf3f7",
+    "url": "/wcovid19/static/css/main.bbcf1485.chunk.css"
+  },
+  {
+    "revision": "1e82cc57024c8dacdd51",
+    "url": "/wcovid19/static/js/2.1b8e2ad1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/wcovid19/static/js/2.1b8e2ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be568a9e89de7dcf3f7",
+    "url": "/wcovid19/static/js/main.5825e285.chunk.js"
+  },
+  {
+    "revision": "5bb7a40e5373dcd72e18",
+    "url": "/wcovid19/static/js/runtime-main.03fdf94b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/wcovid19/static/media/image.d7265326.png"
+  }
+]);
